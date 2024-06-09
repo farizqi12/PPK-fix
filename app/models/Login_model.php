f12576ab702329e2ae70b9cfa73abaf6ee8521d8
Zul Arsyl <133977858+ZULARSYL@users.noreply.github.com>
2024-06-09 09:35:16 +0700

    fix1

--- app/models/Login_model.php
+++ app/models/Login_model.php
@@ -6,8 +6,8 @@ class Login_model{
     public function __construct() {
         $this->db = new Database;
     }
-    public function getUser($username, $password, $role){
-        $this->db->query("SELECT * FROM user WHERE username = :username AND password = :password AND role = :role");
+    public function getUser($username, $password){
+        $this->db->query("SELECT * FROM user WHERE username = :username AND password = :password");
         $this->db->bind('username',$username);
         $this->db->bind('password',$password);
         return $this->db->resultSet();
